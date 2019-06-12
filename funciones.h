1677b050fd1e93a97dc45e4b266daf0af5fab2b9
MauroSMC <39868941+MauroSMC@users.noreply.github.com>
2019-06-12 16:46:42 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void ImprimeMenu();
+void Imprime1();
+void Cambia(int FO,int CO,int FD,int CD);
+bool Perdiste();
+bool Ganaste();
+bool Condicion1(int FO,int CO);
+bool Condicion2(int FD,int CD);
