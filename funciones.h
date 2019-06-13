55255b445e4506fa19dca19d2357fb5d8a8ebbbd
MauroSMC <39868941+MauroSMC@users.noreply.github.com>
2019-06-12 20:54:50 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -3,9 +3,9 @@
 using namespace std;
 
 void ImprimeMenu();
-void Imprime1();
-void Cambia(int FO,int CO,int FD,int CD);
-bool Perdiste();
-bool Ganaste();
-bool Condicion1(int FO,int CO);
-bool Condicion2(int FD,int CD);
+void Imprime1(char M[][8]);
+void Cambia(int FO,int CO,int FD,int CD,char M[][8]);
+bool Perdiste(char M[][8]);
+bool Ganaste(char M[][8]);
+bool Condicion1(int FO,int CO,char M[][8]);
+bool Condicion2(int FD,int CD,char M[][8]);
