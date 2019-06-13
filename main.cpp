4dc51b8c3e3bb4f1d451f1ed58f76f5db3627f7a
MauroSMC <39868941+MauroSMC@users.noreply.github.com>
2019-06-13 14:26:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,12 +12,29 @@ int main() {
                 "0""0""0""0""0""0""0",
                 " "" ""0""0""0"" "" ",
                 " "" ""0""0""0"" "" "};
+  char A[8][8]={" "" ""0""0""0"" "" ",
+                " ""0""0""0""0""0"" ",
+                "0""0""0""+""0""0""0",
+                "0""0""0""0""0""0""0",
+                "0""0""0""0""0""0""0",
+                " ""0""0""0""0""0"" ",
+                " "" ""0""0""0"" "" "};
+  char D[10][10]={" "" "" "" ""0"" "" "" "" ",
+                  " "" "" ""0""0""0"" "" "" ",
+                  " "" ""0""0""0""0""0"" "" ",
+                  " ""0""0""0""0""0""0""0"" ",
+                  "0""0""0""0""0""0""0""0""0",
+                  " ""0""0""0""0""0""0""0"" ",
+                  " "" ""0""0""0""0""0"" "" ",
+                  " "" "" ""0""0""0"" "" "" ",
+                  " "" "" "" ""0"" "" "" "" "};
   int opcion,fO,cO,fD,cD;
   ImprimeMenu();
   cin >> opcion;
   cout << endl;
   switch(opcion){
     case 1:{
+      int MAX=8;
       do{
         Imprime1(M);
         do{
@@ -36,11 +53,38 @@ int main() {
     }
     break;
     case 2:{
+      do{
+        Imprime1(A);
+        do{
+          cout << "Ingrese la posicion de origen\n" << "Fila: "; cin >> fO; cout << "Columna: "; cin >> cO;
+          if(Condicion1(fO,cO,A)==false)
+            cout << "Posicion no valida\n";
+        }while(Condicion1(fO,cO,A)==false);
+        do{
+          cout << "Ingrese la posicion de destino\n" << "Fila: ";cin >> fD; cout << "Columna: "; cin >> cD;
+          if(Condicion2(fD,cD,A)==false)
+            cout << "Posicion no valida\n";
+        }while(Condicion2(fD,cD,A)==false);
+        Cambia(fO,cO,fD,cD,A);
+      }while(Perdiste(A)==true || Ganaste(A)==false);
 
     }
     break;
     case 3:{
-
+      do{
+        Imprime1(M);
+        do{
+          cout << "Ingrese la posicion de origen\n" << "Fila: "; cin >> fO; cout << "Columna: "; cin >> cO;
+          if(Condicion1(fO,cO,A)==false)
+            cout << "Posicion no valida\n";
+        }while(Condicion1(fO,cO,A)==false);
+        do{
+          cout << "Ingrese la posicion de destino\n" << "Fila: ";cin >> fD; cout << "Columna: "; cin >> cD;
+          if(Condicion2(fD,cD,A)==false)
+            cout << "Posicion no valida\n";
+        }while(Condicion2(fD,cD,A)==false);
+        Cambia(fO,cO,fD,cD,A);
+      }while(Perdiste(A)==true || Ganaste(A)==false);
     }
     break;
   }
