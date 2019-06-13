7be74877f654efc382992c5e66228b5d2238c9f6
MauroSMC <39868941+MauroSMC@users.noreply.github.com>
2019-06-12 20:52:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 
 int main() {
+  char M[8][8]={" "" ""0""0""0"" "" ",
+                " "" ""0""0""0"" "" ",
+                "0""0""0""0""0""0""0",
+                "0""0""0""+""0""0""0",
+                "0""0""0""0""0""0""0",
+                " "" ""0""0""0"" "" ",
+                " "" ""0""0""0"" "" "};
   int opcion,fO,cO,fD,cD;
   ImprimeMenu();
   cin >> opcion;
@@ -12,19 +19,19 @@ int main() {
   switch(opcion){
     case 1:{
       do{
-        Imprime1();
+        Imprime1(M);
         do{
           cout << "Ingrese la posicion de origen\n" << "Fila: "; cin >> fO; cout << "Columna: "; cin >> cO;
-          if(Condicion1(fO,cO)==false)
+          if(Condicion1(fO,cO,M)==false)
             cout << "Posicion no valida\n";
-        }while(Condicion1(fO,cO)==false);
+        }while(Condicion1(fO,cO,M)==false);
         do{
           cout << "Ingrese la posicion de destino\n" << "Fila: ";cin >> fD; cout << "Columna: "; cin >> cD;
-          if(Condicion2(fD,cD)==false)
+          if(Condicion2(fD,cD,M)==false)
             cout << "Posicion no valida\n";
-        }while(Condicion2(fD,cD)==false);
-        Cambia(fO,cO,fD,cD);
-      }while(Perdiste()==true || Ganaste()==false);
+        }while(Condicion2(fD,cD,M)==false);
+        Cambia(fO,cO,fD,cD,M);
+      }while(Perdiste(M)==true || Ganaste(M)==false);
 
     }
     break;
