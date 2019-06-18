a78bfc6ae91f6c61458763f101fe87c5a2d08b0b
MauroSMC <39868941+MauroSMC@users.noreply.github.com>
2019-06-18 16:53:23 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -1,76 +1,147 @@
+#include <iostream>
+#include <iomanip>
 #include "funciones.h"
+using namespace std;
 
-void ImprimeMenu(){
-    cout << "Menú - Juego Senku\n" << "-------------------------------\n" << "1. Estilo Ingles\n"<<"2. Estilo Frances\n"<<"3. Estilo Asímetrico\n"<<"--------------------------\n"<<"0. Salir del programa\n\n"<<"Seleccionar opcion: ";}
 
-void Imprime1(char M[][8]){
-    cout << " ";
-    for (int i=1;i<8;i++){
-        cout << setw(4) << i;
-    }
-    cout << endl << endl;
-    for (int i=1;i<8;i++){
-        cout << i;
-        for(int j=0;j<8;j++){
-            cout << setw(4) << M[i-1][j];
+int main() {
+    char M[8][8]={" "" ""0""0""0"" "" ",
+                  " "" ""0""0""0"" "" ",
+                  "0""0""0""0""0""0""0",
+                  "0""0""0""+""0""0""0",
+                  "0""0""0""0""0""0""0",
+                  " "" ""0""0""0"" "" ",
+                  " "" ""0""0""0"" "" "};
+
+    char A[8][8]={" "" ""0""0""0"" "" ",
+                  " ""0""0""0""0""0"" ",
+                  "0""0""0""0""0""0""0",
+                  "0""0""0""+""0""0""0",
+                  "0""0""0""0""0""0""0",
+                  " ""0""0""0""0""0"" ",
+                  " "" ""0""0""0"" "" "};
+
+    char N[8][8]={" ""0""0""0"" "" "" ",
+                  " ""0""0""0"" "" "" ",
+                  " ""0""0""0"" "" "" ",
+                  "0""0""0""0""0""0""0",
+                  "0""0""+""0""0""0""0",
+                  "0""0""0""0""0""0""0",
+                  " ""0""0""0"" "" "" ",
+                  " ""0""0""0"" "" "" "};
+
+    int opcion,fO,cO,fD,cD;
+    ImprimeMenu();
+    cin >> opcion;
+    cout << endl;
+    switch(opcion){
+        case 1:{
+            int jugadas=1;
+            do{
+
+                Imprime1(M);
+                cout << "JUGADA: " << jugadas<<endl;
+                do{
+                    cout << "Ingrese la posicion de origen\n" << "Fila: "; cin >> fO; cout << "Columna: "; cin >> cO;
+                    if(Condicion1(fO,cO,M)==false)
+                        cout << "Posicion no valida\n";
+                }while(Condicion1(fO,cO,M)==false);
+                do{
+                    cout << "Ingrese la posicion de destino\n" << "Fila: ";cin >> fD; cout << "Columna: "; cin >> cD;
+                    if(Condicion2(fD,cD,M)==false)
+                        cout << "Posicion no valida\n";
+                }while(Condicion2(fD,cD,M)==false);
+                Cambia(fO,cO,fD,cD,M);
+                int cont=0;
+                for(int i=0;i<8;i++){
+                    for(int j=0;j<8;j++){
+                        if(M[i][j]=='0'){
+                            cont++;
+                        }
+                    }
+                }
+                if(cont==1){
+                    Imprime1(M);
+                    cout << "GANASTE!!";
+                    break;
+                }
+                cont=0;
+                jugadas++;
+            }while(Ganaste(M)==false||Perdiste(M)==false);
+
         }
-        cout << endl << endl;
-    }    }
-void Cambia(int FO,int CO, int FD, int CD,char M[][8]){
-    M[FO-1][CO-1]='+';
-    M[FD-1][CD-1]='0';
-    M[((FO-1)+(FD-1))/2][((CO-1)+(CD-1))/2]='+';}
+            break;
 
-bool Condicion1(int FO,int CO,char M[][8]){
-    if(M[FO-1][CO-1]=='0'){
-        if((M[FO-1][CO+1]=='+')||(M[FO-1][CO-3]=='+')||(M[FO+1][CO-1]=='+')||(M[FO-3][CO-1]=='+')){
-            return true;
+        case 2:{
+            int jugadas=0;
+            do{
+                Imprime1(A);
+                cout << "JUGADA: " << jugadas<<endl;
+                do{
+                    cout << "Ingrese la posicion de origen\n" << "Fila: "; cin >> fO; cout << "Columna: "; cin >> cO;
+                    if(Condicion1(fO,cO,A)==false)
+                        cout << "Posicion no valida\n";
+                }while(Condicion1(fO,cO,A)==false);
+                do{
+                    cout << "Ingrese la posicion de destino\n" << "Fila: ";cin >> fD; cout << "Columna: "; cin >> cD;
+                    if(Condicion2(fD,cD,A)==false)
+                        cout << "Posicion no valida\n";
+                }while(Condicion2(fD,cD,A)==false);
+                Cambia(fO,cO,fD,cD,A);
+                int cont=0;
+                for(int i=0;i<8;i++){
+                    for(int j=0;j<8;j++){
+                        if(M[i][j]=='0'){
+                            cont++;
+                        }
+                    }
+                }
+                if(cont==1){
+                    Imprime1(M);
+                    cout << "GANASTE!!";
+                    break;
+                }
+                cont=0;
+                jugadas++;
+            }while(Perdiste(A)==false || Ganaste(A)==false);
         }
-        else
-            return false;
+            break;
+
+        case 3:{
+            do{
+                int jugadas=0;
+                Imprime1(N);
+                cout << "JUGADA: " << jugadas<<endl;
+                do{
+                    cout <<"Ingrese la posición de origen\n" << "Fila: "; cin >> fO;
+                    cout << "Columna: "; cin >> cO;
+                    if(Condicion1(fO,cO,N)==false)
+                        cout << "Posicion no valida\n";
+                }while(Condicion1(fO,cO,N)==false);
+                do{
+                    cout << "Ingrese la posicion de destino\n" << "Fila: ";cin >> fD;
+                    cout << "Columna: "; cin >> cD;
+                    if(Condicion2(fD,cD,N)==false)
+                        cout << "Posicion no valida\n";
+                }while(Condicion2(fD,cD,N)==false);
+                Cambia(fO,cO,fD,cD,N);
+                int cont=0;
+                for(int i=0;i<8;i++){
+                    for(int j=0;j<8;j++){
+                        if(M[i][j]=='0'){
+                            cont++;
+                        }
+                    }
+                }
+                if(cont==1){
+                    Imprime1(M);
+                    cout << "GANASTE!!";
+                    break;
+                }
+                cont=0;
+                jugadas++;
+            }while(Perdiste(N)==true || Ganaste(N)==false);
+        }
+            break;
     }
 }
-bool Condicion2(int FD,int CD,char M[][8]){
-    if(M[FD-1][CD-1]=='+')
-        return true;
-    else
-        return false;}
-
-bool Perdiste(char M[][8]){
-    int cont=0,cont2=0;
-    for (int i=0;i<8;i++){
-        for (int j=0;j<8;j++){
-            if(M[i][j]=='0'){
-                if(M[i][j-1]=='+'|| M[i][j+1]=='+' || M[i-1][j]=='+'|| M[i+1][j]=='+'||M[i-1][j+1]==){
-                    cont2++;
-                }
-            }
-        }
-    }
-    for(int i=0;i<8;i++){
-        for(int j=0;j<8;j++) {
-            if (M[i][j] == '0') {
-                cont++;
-            }
-        }
-    }
-    if (cont==cont2){
-        return true;
-    }
-    else
-        return false;}
-
-bool Ganaste(char M[][8]){
-    int cont=0;
-    for(int i=0;i<8;i++){
-        for(int j=0;j<8;j++){
-            if(M[i][j]=='0'){
-                cont++;
-            }
-        }
-    }
-    if(cont==1){
-        return true;
-    }
-    else
-        return false;}
